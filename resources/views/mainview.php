71bec0bc6e846b981bf5a4bec8435f6d25d0add4
tsubasa shimizu <wingmacbook@gmail.com>
2022-11-30 22:21:54 +0900

    building the framework

--- resources/views/mainview.php
+++ resources/views/mainview.php
@@ -2,8 +2,169 @@
 <html>
 <head>
     <title>contact</title>
+    <style>
+      .backbround{
+        
+        background-image: url(resource/views/images/image2 2.jpeg);
+        background: #EEEEEE	;
+        opacity: 1;
+        /* background-repeat: no-repeat; */
+        width: all;
+      }
+
+      .p{
+        /* width:"100"; */
+
+      }
+
+
+
+
+      .flame{
+        height: 90vh;
+        /* color:white ; */
+        /* background: #EEEEEE	;
+        opacity: 1; */
+      }
+
+      .Page_Title{
+        text-align: center;
+        height: 90vh;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+      }
+
+
+      .Page_Profile{
+        padding:50px;
+        height: 90vh;
+        /* color:white ; */
+
+      }
+      .Page_Skill{
+        padding:50px;
+        height: 90vh;
+        /* color:white ; */
+
+      }
+      .Page_Work{
+        padding:50px;
+        height: 90vh;
+        /* color:white ; */
+
+      }
+      .Page_Work{
+        padding:50px;
+        height: 90vh;
+        /* color:white ; */
+
+      }
+      .Page_Contact{
+        padding:50px;
+        height: 90vh;
+        /* color:white ; */
+
+      }
+
+            /* html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 90vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 90vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 13px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            } */
+        </style>
 </head>
 <body>
-    <h1>Hello World!!</h1>
+<div class = backbround>
+  <p>
+  <img src="/images/image2 2.jpeg" width="50%">
+  </p>
+
+  <div class = image></div>
+    <div class = flame>
+
+        <div class = Page_Title>
+          <div class = Box_Title>
+            <h1>TSUBASA SHIMIZU's Portfolio</h1>
+            <h2>↓Scroll↓</h2>
+          </div>
+        </div>
+        
+        <div class = Page_Profile>
+          <h1>Profile</h1>
+          <div class = Profile_contents>
+            生年月日：1994年9月生まれ<br><br>
+            趣味：一人旅<br><br><br><br>
+            ＊＊＊＊年＊月：<br><br>
+            ＊＊＊＊年＊月：<br><br>
+            ＊＊＊＊年＊月：<br><br>
+            ＊＊＊＊年＊月：<br><br><br>
+          </div>
+        </div>
+
+        <div class = Page_Skill>
+          <h1>Skill</h1>
+          <div class = Skill_contents></div>
+        </div>
+
+        <div class = Page_Work>
+          <h1>Work</h1>
+          <div class = Work_image></div>
+          <div class = Work_contents></div>
+        </div>
+
+        <div class = Page_Contact>
+          <h1>Contact</h1>
+          <div class = Contact_image></div>
+          <div class = Contact_contents></div>
+      </div>
+    </div>
+  </div>
+</div>
 </body>
 </html>
